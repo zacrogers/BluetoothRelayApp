db2d827889eaddd32d0ba8f16a0deb7c5ee74fc1
rogerszach <rogerszach@myvuw.ac.nz>
2020-11-16 11:04:36 +1300

    Add project files.

---
+++ BluetoothRelayApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BluetoothRelayApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button4_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolbarItem_Activated(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new BtSelectPage());
+        }
+
+
+    }
+}
